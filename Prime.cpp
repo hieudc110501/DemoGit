309dd73c1de50841bec17ef8580613165936b6c5
Hiếu trương <truongminhhieu152@gmai.com>
2021-09-06 13:07:50 +0700

    add file

---
+++ Prime.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define FOR(i,a,b) for(int i = a; i <= b; i ++)
+using namespace std;
+
+bool checkPrime(int number)
+{
+	if(number < 2) return false;
+	for(int i = 2; i <= sqrt(number); i++)
+		if(number % i == 0)
+			return false;
+	return true;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int nInput;
+	cin >> nInput;
+	int nArray;
+	while(nInput--)
+	{
+		cin >> nArray;
+		if(checkPrime(nArray))
+			cout << nArray << " ";
+	}
+}
+
